17192ad0a3c4a8a3008b139f4f49277e164efb56
Arhsota <arhsota@gmail.com>
2022-08-22 16:45:26 +0300

    test31

---
+++ imtwebEn/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import "../styles/myheader.css";
+// import market from "../images/google_market.png";
+// import flag from "../images/unionjack.png";
+
+const Header = () => {
+  return (
+    <header className="myheader">
+      {/* <img src={market} alt="Logo" width="100" /> */}
+      {/* <img src={flag} alt="Logo" width="50" /> */}
+      {/* <h1>Ваше здоровье</h1> */}
+
+      <a href="№" target="_blank">
+        {" "}
+        Desription
+      </a>
+    </header>
+  );
+};
+export default Header;
