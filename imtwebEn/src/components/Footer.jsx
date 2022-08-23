499d68be9527cc4753c17377605b162541f80cdf
Andy <arhsota@gmail.com>
2022-08-23 15:34:24 +0300

    Add files via upload

---
+++ imtwebEn/src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "../styles/myfooter.css";
+// import mypic from "../images/Health.png";
+import market from "../images/google_market.png";
+import flag from "../images/unionjack.png";
+
+const Footer = () => {
+  return (
+    <footer className="myfooter">
+      {/* <img src={mypic} alt="Logo" width="200" /> */}
+      <div>
+        <h3 className="head3">
+          This application Body Mass Index (BMI) is not medical product, but
+          only for your information
+        </h3>
+      </div>
+      <div className="lastString">
+        <h3 className="head3">All Rights Reserved. Copyright (c)2022 </h3>
+        <a
+          href="https://play.google.com/store/apps/details?id=com.arhsota.android.imt"
+          target="_blank"
+          className="googleMarket"
+        >
+          {" "}
+          <img src={market} alt="Google Market" width="100" />
+        </a>
+        <a href="https://imt-web-en.vercel.app/" target="_blank">
+          {" "}
+          <img src={flag} alt="Union Jack" width="50" />
+        </a>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
---
+++ imtwebEn/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "../styles/myheader.css";
+import "./description.jsx";
+// import market from "../images/google_market.png";
+// import flag from "../images/unionjack.png";
+
+const Header = () => {
+  return (
+    <header className="myheader">
+      {/* <img src={market} alt="Logo" width="100" /> */}
+      {/* <img src={flag} alt="Logo" width="50" /> */}
+      {/* <h1>Ваше здоровье</h1> */}
+
+      <a href="#" onClick={" "}>
+        {" "}
+        Description
+      </a>
+    </header>
+  );
+};
+export default Header;
